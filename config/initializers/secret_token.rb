a59f5e3c70d90a8dd8f0a41dac3149d9627c9f3e
dgsgreffenius <Stefan.Greffenius@DealGecco.com>
2012-09-26 15:22:14 +0200

    Init of a new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeUntouched::Application.config.secret_token = '39917da1316d7c22118c222b054aef8953265f94e6f1311a9074c576404c1e87be70ff9c995efc32096dec3aa29ec215ea12ba04a6fbb723a4916df25c027447'
